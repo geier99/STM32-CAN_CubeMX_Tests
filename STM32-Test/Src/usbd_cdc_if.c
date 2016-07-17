0ff6fd095625362300d6ee0c115afa9d771f3eb5
Andreas <geier99@gmx.de>
2016-07-17 23:01:53 +0200

    - bug Fix  : Hardfault then pressing "send" Button in CAN-Hakcer

--- STM32-Test/Src/usbd_cdc_if.c
+++ STM32-Test/Src/usbd_cdc_if.c
@@ -60,8 +60,8 @@
 /* USER CODE BEGIN PRIVATE_DEFINES */
 /* Define size for the receive and transmit buffer over CDC */
 /* It's up to user to redefine and/or remove those define */
-#define APP_RX_DATA_SIZE  4
-#define APP_TX_DATA_SIZE  4
+#define APP_RX_DATA_SIZE  2048
+#define APP_TX_DATA_SIZE  2048
 /* USER CODE END PRIVATE_DEFINES */
 /**
   * @}
